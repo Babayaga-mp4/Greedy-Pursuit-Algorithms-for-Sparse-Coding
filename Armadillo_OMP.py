2fb02a0ab0f70650c51df1c727dd4253f6a31a49
babayaga-mp4 <tsyrraina@gmail.com>
2021-06-13 16:56:24 +0530

    Refining

--- Armadillo_OMP.py
+++ Armadillo_OMP.py
@@ -9,19 +9,19 @@ def omp(dictionary, image, variance):
     X_hat = mat(column, 1, fill.zeros)
     s = []
     error_power = pow(normalise(r), 2) / column
-    print(error_power >= variance)
+    # print(error_power >= variance)
     while error_power >= variance:
         next_col = index_max(dictionary.t() * r)[0]
         s.append(next_col)
         A_reduced = join_rows(A_reduced, dictionary[:, next_col])
         Xs = pinv(A_reduced) * image
-        # counter = 0
-        alpha = mat(s)
-        X_hat[alpha] = Xs
-        X_hat.print()
-        # for index in s:
-        #     X_hat[index] = Xs[counter]
-        #     counter += 1
+        counter = 0
+        # alpha = mat(s)
+        # X_hat[alpha] = Xs
+        # X_hat.print()
+        for index in s:
+            X_hat[index] = Xs[counter]
+            counter += 1
 
         b_hat = (dictionary * X_hat)
         r = image - b_hat
@@ -39,12 +39,12 @@ A = mat(rows, cols, fill.randn)
 A = normalise(A, 2, 1)
 X = mat(rand(cols, 1, density=k / cols).todense())  # <UPDATE: Specify the limits Uniform Dst> --> Resolved in Scratch 3
 b = A * X
-SNR = 2
+SNR = 50
 var = (norm(b) ** 2 / rows) / (10 ** (SNR / 10))
 n = mat(rows, 1, fill.randn) * var
 b_n = b + n
 X_out = omp(A, b_n, var)
-# join_rows(X, X_out).print()
+join_rows(X, X_out).print()
 # plt.stem(X_hat)
 # plt.show()
 # plt.stem(X)
