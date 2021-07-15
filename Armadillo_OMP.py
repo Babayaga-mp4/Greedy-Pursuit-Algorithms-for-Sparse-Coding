2f355a71637987591f76efc01e586d45f79e7d9f
babayaga-mp4 <tsyrraina@gmail.com>
2021-07-15 12:29:57 +0530

    Refining

--- Armadillo_OMP.py
+++ Armadillo_OMP.py
@@ -3,18 +3,22 @@ from pyarma import *
 
 def omp(dictionary, image, variance):
     r = image
+    samples = image.n_cols
     column = dictionary.n_cols
     row = dictionary.n_rows
     A_reduced = mat(row, 0, fill.none)
-    X_hat = mat(column, 1, fill.zeros)
+    X_hat = mat(column, samples, fill.zeros)
     s = []
     error_power = pow(normalise(r), 2) / column
     # print(error_power >= variance)
     while error_power >= variance:
         next_col = index_max(dictionary.t() * r)[0]
+        # print(max(dictionary.t() * r))
+        # print(next_col)
         s.append(next_col)
         A_reduced = join_rows(A_reduced, dictionary[:, next_col])
         Xs = pinv(A_reduced) * image
+        # print(size(Xs))
         counter = 0
         # alpha = mat(s)
         # X_hat[alpha] = Xs
@@ -26,22 +30,25 @@ def omp(dictionary, image, variance):
         b_hat = (dictionary * X_hat)
         r = image - b_hat
         error_power = (norm(r) ** 2) / column
+        # error_power = pow(normalise(r), 2) / column Study this statement
     return X_hat
 
 
 from scipy.sparse import rand
 import matplotlib.pyplot as plt
 
-rows = 20
-cols = 50
+rows = 30
+cols = 60
 k = 6
 A = mat(rows, cols, fill.randn)
 A = normalise(A, 2, 1)
-X = mat(rand(cols, 1, density=k / cols).todense())  # <UPDATE: Specify the limits Uniform Dst> --> Resolved in Scratch 3
+n_samples = 4
+X = mat(rand(cols, n_samples, density=k / cols).todense())  # <UPDATE: Specify the limits Uniform Dst> --> Resolved in Scratch 3
 b = A * X
 SNR = 50
 var = (norm(b) ** 2 / rows) / (10 ** (SNR / 10))
 n = mat(rows, 1, fill.randn) * var
+# print(size(var))
 b_n = b + n
 X_out = omp(A, b_n, var)
 join_rows(X, X_out).print()
@@ -49,3 +56,5 @@ join_rows(X, X_out).print()
 # plt.show()
 # plt.stem(X)
 # plt.show()
+
+# Study the difference between norm and normalise
--- SOMP.py
+++ SOMP.py
@@ -4,12 +4,13 @@ from numpy import linalg as LA
 
 def omp(A, b, variance, *args):
     r = b
+    samples = b.shape[1]
     cols = A.shape[1]
     rows = A.shape[0]
     if args == (): k = cols / 2
     else: k = args[0]
     A_reduced = np.empty((rows, 0))  # As Matrix
-    X = np.zeros((cols, 1))
+    X = np.zeros((cols, samples))
     s = []
     error_power = (LA.norm(r)**2) / cols
     while error_power >= variance and k > 0:
@@ -23,7 +24,7 @@ def omp(A, b, variance, *args):
         r = b - b_hat
         error_power = (LA.norm(r) ** 2 )/ cols
         k -= 1
-    return X
+    return X, error_power
 
 
 from scipy.sparse import rand
@@ -32,17 +33,21 @@ import warnings
 warnings.filterwarnings("ignore", category=np.VisibleDeprecationWarning)
 rows = 20
 cols = 50
-k = 4
+k = 5
 A = np.random.randn(rows, cols)
 for i in range(cols):
     A[:,i] = A[:,i]/LA.norm(A[:,i])
-X = rand(cols, 1, density=k / cols).todense()                           # <UPDATE: Specify the limits Uniform Dst> --> Resolved in Scratch 3
+n_samples = 4
+X = rand(cols, n_samples, density=k / cols).todense()                           # <UPDATE: Specify the limits Uniform Dst> --> Resolved in Scratch 3
 b = A * X
-SNR = 50
+SNR = 30
 variance = (LA.norm(b) ** 2 / rows) / (10 ** (SNR / 10))
 n = np.random.randn(rows, 1) * np.sqrt(variance)
 b_n = b + n
-X_hat = omp(A, b_n, variance, k)
-print(np.column_stack((X, X_hat)))
+X_hat, resi = omp(A, b_n, variance, k)
+# print(np.column_stack((X, X_hat)))
+print(X, X.shape)
+print(X_hat, X_hat.shape)
+print(resi)
 # plt.stem(X_hat, use_line_collection=True)
 # plt.show()
